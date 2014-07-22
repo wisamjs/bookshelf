a72d1cc4f90e2454fbabce14b4443ec9cf5d50f1
wzaghal <wisamzaghal@gmail.com>
2014-07-22 16:56:31 -0400

    add cors comment

--- routes.js
+++ routes.js
@@ -2,14 +2,11 @@
 module.exports = {
 	load: function(app, mongoose, Book){
 
+		//middleware to enable CORS
 		app.all('*', function(req, res, next) {
     		res.header("Access-Control-Allow-Origin", "*");
     		res.header("Access-Control-Allow-Headers", "X-Requested-With");
     		next();
-		});
-		
-		app.get('/', function(req, res, next){
-			res.json({'message':'welcome to our api'});
 		});
 
 		//add book
