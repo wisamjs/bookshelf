cdc7844524e5731952ac9ecb0d4fcd52ba9f2d03
wzaghal <wisamzaghal@gmail.com>
2014-07-22 17:24:39 -0400

    view books using angular get req and ng-repeat

--- app/controllers/MainCtrl.js
+++ app/controllers/MainCtrl.js
@@ -1,2 +1,16 @@
-myApp.controller('MainController',['$scope', function($scope){
+myApp.controller('MainController',
+	['$scope','$http', function($scope,$http){
+
+		$scope.books;
+		$http.get('http://localhost:3000/books')
+
+			.success( function(data, status, headers, config){
+				$scope.books = data;
+				console.log(data);
+			})
+
+			.error(function(data, status, headers, config){
+				console.log('error');
+
+			});
 }]);
