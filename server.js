44fa713e849dd3c6dc665c6ba826eb77aa685223
wzaghal <wisamzaghal@gmail.com>
2014-07-25 19:00:42 -0400

    add heroku and mongodb server

--- server.js
+++ server.js
@@ -20,7 +20,7 @@ app.use(express.static(path.join(__dirname, 'app')));
 
 
 //Database Connection
-mongoose.connect('localhost/newDB');
+mongoose.connect('process.env.MONGODB_URL');
 
 mongoose.connection.on('connected',function(){
 	console.log('Connection successful');
