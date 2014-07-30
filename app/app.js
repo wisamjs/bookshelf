003d2f47e8cc339faa2ad37d6ed00b3e9f13c3a5
wzaghal <wisamzaghal@gmail.com>
2014-07-29 22:21:31 -0400

    move content to view

--- app/app.js
+++ app/app.js
@@ -1,2 +1,19 @@
 'use strict';
-var myApp = angular.module('MyApp',[]);
+var myApp =
+
+angular.module('MyApp', ['ngRoute'])
+ .config(['$locationProvider', '$routeProvider', function($locationProvider, $routeProvider) {
+
+
+  	/*locationProvider is a built in
+  	AngularJs service for configuring application
+  	linking paths.
+  	*/
+  	$locationProvider.html5Mode(true);
+
+  	//Routes
+  	$routeProvider
+	  	.when('/', {
+		    templateUrl: 'views/home.html'
+		  });
+  }]);
